82c6fc2b495c635b132434420f0d0f9d662b8113
Simon C <corbetsimon@gmail.com>
2019-05-22 14:37:00 +0200

    modif condition

--- PHP/Conditions.php
+++ PHP/Conditions.php
@@ -55,13 +55,17 @@
                 <form>
                     <h1> Nos conditions </h1>
                     <hr/>
-<p>Vous pouvez avoir recours à nos services pour toutes sortes de raisons : pour rechercher et partager des données, pour communiquer avec d’autres personnes ou pour créer de nouveaux contenus.
-    En partageant vos données avec nous, vous nous permettez d’augmenter la qualité de ces services – en améliorant la pertinence des annonces et des résultats qui vous sont proposés, en vous aidant à établir des contacts ou en accélérant le partage de vos données. Comme vous utilisez nos services, nous souhaitons que vous compreniez comment nous utilisons vos données et de quelles manières vous pouvez protéger votre vie privée.
-    Notre politique de confidentialité explique :
-    Les données que nous collectons et les raisons de cette collecte ;
-    La façon dont nous utilisons ces données ;
-    Nous nous efforçons d’être le plus clair possible.
-    Toutefois, si vous n’êtes pas familier avec les termes comme témoins, adresses IP, balises pixel et navigateurs, renseignez-vous au préalable sur ces termes clés. Ainsi, que vous soyez un nouveau visiteur ou un habitué, prenez le temps de découvrir notre site et, si vous avez des questions, n’hésitez pas à communiquer avec nous.</p>
+<br />
+		<p>Dernière modification : 22 mai 2019</p>
+
+		<p>Vous pouvez avoir recours à nos services pour toutes sortes de raisons : pour rechercher et partager des données, pour communiquer avec d’autres personnes ou pour créer de nouveaux contenus.</p>
+		<p>En partageant vos données avec nous, vous nous permettez d’augmenter la qualité de ces services – en améliorant la pertinence des annonces et des résultats qui vous sont proposés, en vous aidant à établir des contacts ou en accélérant le partage de vos données. Comme vous utilisez nos services, nous souhaitons que vous compreniez comment nous utilisons vos données et de quelles manières vous pouvez protéger votre vie privée.</p>
+
+		<p>Notre politique de confidentialité explique :</p>
+		<p>Les données que nous collectons et les raisons de cette collecte;</p>
+		<p>La façon dont nous utilisons ces données;</p>
+			Nous nous efforçons d’être le plus clair possible. 
+		<p>Toutefois, si vous n’êtes pas familier avec les termes comme témoins, adresses IP, balises pixel et navigateurs, renseignez-vous au préalable sur ces termes clés. Ainsi, que vous soyez un nouveau visiteur ou un habitué, prenez le temps de découvrir notre site et, si vous avez des questions, n’hésitez pas à<a href="contact.php"> communiquer avec nous.</a></p>	
                 </form><!-- On met un bouton pour afficher plus de critiques -->
 			</section>
 		</div>
